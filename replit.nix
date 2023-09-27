f7b05b296ce4f74674e3ed9e9ea073aacf7ebe55
BerkahEsport <91768824+BerkahEsport@users.noreply.github.com>
2023-09-27 21:30:36 +0700

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,39 +1,11 @@
 { pkgs }: {
-
-    deps = [
-
-        pkgs.nodejs
-
-        pkgs.nodePackages.typescript
-
-        pkgs.ffmpeg
-
-        pkgs.imagemagick
-
-        pkgs.git
-
-        pkgs.neofetch
-
-        pkgs.libwebp
-
-        pkgs.speedtest-cli
-
-        pkgs.wget
-
-        pkgs.yarn
-
-        pkgs.libuuid
-
-    ];
-
-    env = {
-
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-
-            pkgs.libuuid
-
-        ];
-
-    };
-
+deps = [
+    pkgs.nodejs_20
+    pkgs.neofetch
+    pkgs.imagemagick
+    pkgs.speedtest-cli
+    pkgs.jellyfin-ffmpeg
+    pkgs.git
+    pkgs.python311Packages.python
+];
 }
