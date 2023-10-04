5220a93eeab820ed6781d774ccfe0c4aae82deca
BerkahEsport <91768824+BerkahEsport@users.noreply.github.com>
2023-10-04 19:46:05 +0700

    Update

--- event/message.js
+++ event/message.js
@@ -11,6 +11,10 @@ import { format } from "util"
 import { promisify } from 'util'
 import cron from "node-cron"
 import didyoumean from "didyoumean"
+import { cpus as _cpus, totalmem, freemem } from 'os'
+import os from 'os'
+import { performance } from 'perf_hooks'
+import { sizeFormatter } from 'human-readable'
 
 export default async function Message(conn, m, message) {
     try {
@@ -275,12 +279,87 @@ ${rres.data}`.trim())
             }
             break
             case "ping": {
-                const moment = (await import("moment-timezone")).default
-                const calculatePing = function (timestamp, now) {
-                    return moment.duration(now - moment(timestamp * 1000)).asSeconds();
+                let format = sizeFormatter({
+                  std: 'JEDEC', // 'SI' (default) | 'IEC' | 'JEDEC'
+                  decimalPlaces: 2,
+                  keepTrailingZeroes: false,
+                  render: (literal, symbol) => `${literal} ${symbol}B`,
+                })
+                    let _muptime
+                    if (process.send) {
+                      process.send('uptime')
+                      _muptime = await new Promise(resolve => {
+                        process.once('message', resolve)
+                        setTimeout(resolve, 1000)
+                      }) * 1000
+                    }
+                    let muptime = clockString(_muptime)
+                  const used = process.memoryUsage()
+                  const cpus = _cpus().map(cpu => {
+                    cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
+                    return cpu
+                  })
+                  const cpu = cpus.reduce((last, cpu, _, { length }) => {
+                    last.total += cpu.total
+                    last.speed += cpu.speed / length
+                    last.times.user += cpu.times.user
+                    last.times.nice += cpu.times.nice
+                    last.times.sys += cpu.times.sys
+                    last.times.idle += cpu.times.idle
+                    last.times.irq += cpu.times.irq
+                    return last
+                  }, {
+                    speed: 0,
+                    total: 0,
+                    times: {
+                      user: 0,
+                      nice: 0,
+                      sys: 0,
+                      idle: 0,
+                      irq: 0
+                    }
+                  })
+                
+                  let old = performance.now()
+                  await m.reply(`*T e s t i n g. . .*`)
+                  let neww = performance.now()
+                  let speed = neww - old
+                  await conn.reply(m.from,`*𝐒 𝐏 𝐄 𝐄 𝐃*
+                ${Math.round(neww - old)} ms
+                ${speed} ms
+                
+                *ʀ ᴜ ɴ ᴛ ɪ ᴍ ᴇ*
+                ${muptime}
+                ${readMore}
+                
+                *ꜱᴇʀᴠᴇʀ*
+                *🛑 ʀᴀᴍ:* ${format(totalmem() - freemem())} / ${format(totalmem())}
+                *🔵 ꜰʀᴇᴇʀᴀᴍ:* ${format(freemem())}
+                
+                *💻 ᴘʟᴀᴛꜰᴏʀᴍ:* ${os.platform()}
+                *🧿 ꜱᴇʀᴠᴇʀ:* ${os.hostname()}
+                ${readMore}
+                *ɴᴏᴅᴇ-ᴊꜱ ᴍᴇᴍᴏʀʏ ᴜꜱᴀɢᴇ*
+                ${'```' + Object.keys(used).map((key, _, arr) => `${key.padEnd(Math.max(...arr.map(v => v.length)), ' ')}: ${format(used[key])}`).join('\n') + '```'}
+                
+                ${cpus[0] ? `_ᴛᴏᴛᴀʟ ᴄᴘᴜ ᴜꜱᴀɢᴇ_
+                ${cpus[0].model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}
+                
+                _ᴄᴘᴜ ᴄᴏʀᴇ(ꜱ) ᴜꜱᴀɢᴇ (${cpus.length} ᴄᴏʀᴇ ᴄᴘᴜ)_
+                ${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}`).join('\n\n')}` : ''}
+                `, m)
                 }
-                m.reply(`*Ping :* *_${calculatePing(m.timestamp, Date.now())} second(s)_*`)
-            }
+                
+                const more = String.fromCharCode(8206)
+                const readMore = more.repeat(4001)
+                
+                function clockString(ms) {
+                  let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
+                  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
+                  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+                  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+                  return [d, ' *Days ☀️*\n ', h, ' *Hours 🕐*\n ', m, ' *Minute ⏰*\n ', s, ' *Second ⏱️* '].map(v => v.toString().padStart(2, 0)).join('')
+                }
             break
             case "quoted": case "q": {
                 const { Serialize } = (await import("../lib/serialize.js"))
@@ -489,7 +568,7 @@ ${rres.data}`.trim())
             }
             break
             case "kisahnabi": {
-if (!m.args[0]) return conn.reply(m.from, `ʜᴀʀᴀᴘ ᴍᴀꜱᴜᴋᴀɴ ɴᴀᴍᴀ ɴᴀʙɪ\n\nᴄᴏɴᴛᴏʜ: .kisahnabi ᴍᴜʜᴀᴍᴍᴀᴅ`,m);
+if (!m.args[0]) return m.reply(m.from, `ʜᴀʀᴀᴘ ᴍᴀꜱᴜᴋᴀɴ ɴᴀᴍᴀ ɴᴀʙɪ\n\nᴄᴏɴᴛᴏʜ: .kisahnabi ᴍᴜʜᴀᴍᴍᴀᴅ`,m);
 let data = [ 'Adam', 'Idris', 'Nuh', 'Hud', 'Sholeh',
 'Ibrahim', 'Ismail', 'Luth', 'Ishaq', 'Yaqub',
 'Yusuf', "Syu'aib", 'Ayyub', 'Dzulkifli', 'Musa',
@@ -526,7 +605,7 @@ conn.sendMessage(m.from, {
 };
 break
 case "doa": {
-    if (!m.args[0]) return conn.reply("ᴍᴀꜱᴜᴋᴋᴀɴ ɴᴀᴍᴀ ᴅᴏᴀɴʏᴀ ᴀᴘᴀ?")
+    if (!m.args[0]) return m.reply("ᴍᴀꜱᴜᴋᴋᴀɴ ɴᴀᴍᴀ ᴅᴏᴀɴʏᴀ ᴀᴘᴀ?")
 conn.doa = conn.doa ? conn.doa : {}
 let doaseharihari = await (await fetch("https://raw.githubusercontent.com/BerkahEsport/api-be/main/data/islam/lainya/doaharian.json")).json()
 let data = doaseharihari.data.map((v,i) => `\n${i+1}. ${v.title}`)
