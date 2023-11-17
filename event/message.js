416c685d5450b055f02166e8f4eb1b72f4523aeb
BerkahEsport <91768824+BerkahEsport@users.noreply.github.com>
2023-11-17 13:01:07 +0700

    Update

--- event/message.js
+++ event/message.js
@@ -831,17 +831,17 @@ m.reply(result[0].audio, {asDocument: true, fileName: result[0].nama})
 break
 // <===== Category Tools =====>
 case 'qc': case 'quickchat': {
+    let skizo = Func.random(["doyansharing", "y6rsxtbase"])
     let who = m.hasQuotedMsg ? m.mentions[0] : m.fromMe ? conn.user.jid : m.sender
     let name = await conn.getName(who)
-    var fakec, text
+    let text = m.hasQuotedMsg ? m.quoted.text : m.text
+    var fakec
     let avatar = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph//file/c4044a0d3b4cc8b8dc2dd.jpg')
     try {
         fakec = "https://mfarels.my.id/api/fakechat-wa?nama=" + encodeURIComponent(name) + "&text=" + encodeURIComponent(text) + "&no=" + encodeURIComponent(who.split("@")[0])
-        text = m.hasQuotedMsg ? m.quoted.text : m.text
     } catch (e) {
         try {
-            fakec = `https://skizo.tech/api/fakechat?text=${encodeURIComponent(text)}&username=${name}&avatar=${avatar}&apikey=y6rsxtbase`
-            text = m.hasQuotedMsg ? m.quoted.text : m.text
+            fakec = `https://skizo.tech/api/fakechat?text=${encodeURIComponent(text)}&username=${name}&avatar=${avatar}&apikey=${skizo}`
         } catch (e) {
             await m.reply(e)
         }
